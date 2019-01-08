3ab61ad34456ccbe1029e6a335b45a108017c921
faqtr <d.faqtr@gmail.com>
2019-01-08 16:31:52 +0200

    навигация

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +7,32 @@
     <link rel="stylesheet" href="css\bootstrap-reboot.min.css">
     <link rel="stylesheet" href="css\bootstrap-grid.css">
     <link rel="stylesheet" href="css\bootstrap.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="js\bootstrap.bundle.min.js"></script>
     <script src="js\bootstrap.min.js"></script>
+
     <title>EngLear</title>
 </head>
-<body>
-    <header>
-        <nav>
+<body class="container">
+    <header class="row">
+        <nav class="navbar navbar-expand-sm navbar-light col-sm-12">
+            <a class="navbar-brand" href="#">englear</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#">Курсы</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Уроки</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Отзывы</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
+
     <section>
     форма связи
     </section>
